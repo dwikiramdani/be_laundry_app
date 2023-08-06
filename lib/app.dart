0307b77a1f807c5b4795073bb85a422d41e9aadd
dwikiiramdani <official.dwikiramdani@gmail.com>
2023-08-06 12:30:38 +0700

    Code Improvement

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:be_laundry/view/home_view.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeView(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
