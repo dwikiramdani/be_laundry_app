19749cf684390862335e08f12a725e29fd902ee5
dwikiiramdani <official.dwikiramdani@gmail.com>
2023-08-07 17:49:33 +0700

    Implement provider 6.0.5 library

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,25 @@
 import 'package:be_laundry/view/home_view.dart';
+import 'package:be_laundry/view/model/home_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (context) {
+        HomeModel();
+      },
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomeView(title: 'Flutter Demo Home Page'),
       ),
-      home: const HomeView(title: 'Flutter Demo Home Page'),
     );
   }
 }
